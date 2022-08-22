f03651270a7b324ba6d465b98f7134dcf03c5b1f
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-22 11:09:50 -0400

    Implemented minimum float value

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	
+        float myMinFloatValue = Float.MIN_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+
 
         
     }
